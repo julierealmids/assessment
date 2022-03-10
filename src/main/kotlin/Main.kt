34e0c66ad6f10e2e6f24d1d43dbf66de7544a482
julierealmids <jullietnakayizaa@gmail.com>
2022-03-10 08:51:27 +0300

    first assesment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+fun main() {
+    var p ="Julliet"
+    var z = "22"
+    println("my name is $p,i am $z years old")
+    var s =("variables")
+    println(s.length)
+
+    var modulus = (5%3)
+    var result = (5%3)
+    println(result)
+
+
+
+}
+fun name(name:String){
+    var v = "codeHive"
+    println(v[5])
+    println(v[6])
+    println(v[7])
+    println(v[8])
+
+}
+fun numbers(numbers:Int){
+    var a = 5
+    var f = 3
+    var modulus = 5%3
+    println(modulus)
+}
+fun myname(name:String,age:Int){
+    var p = "Julliet"
+    var z = 22
+    println("my name is $p,i am $z years old")
+
+
+}
+fun family(family:String) {
+    var s = ("variables")
+    println(s.length)
+
+
+}
+
